ef47a0dbea9e39ec141119e4a951282a451eb610
Giri Prasad <giri@giri-webdev.net>
2019-01-27 17:01:51 +0530

    Published  livedemo @ http://livedemo.giri-webdev.net

--- giri-webdev-livedemo/App_Start/RouteConfig.cs
+++ giri-webdev-livedemo/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace giri_webdev_livedemo
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "PagingProcedure", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
